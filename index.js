f57aebe1a4b2f5e22a92f1be860f4e0108a81f62
Margaret Pope <mmp298@gmail.com>
2023-11-26 22:45:39 -0600

    final attempt

--- index.js
+++ index.js
@@ -9,7 +9,24 @@ const pipe = (...fns) => firstArg => fns.reduce((returnValue, fn) => fn(returnVa
 const makeTag = tag => str => `<${tag}>${str}</${tag}>`
 
 // complete this function
-const makePoemHTML = () => {}
+const makePoemHTML = (poem) => {
+  const {author, lines, title} = poem[0]
+  console.log(poem)
+  const poemTitle = makeTag('h2')(`${title}`)
+  const byLine = makeTag('em')('by ' + `${author}`)
+  const h3 = makeTag('h3')(byLine)
+
+  const br = makeTag('br')
+  const lineBr = lines
+    .map(br)
+    .join('')
+
+  const stanza = makeTag('p')(lineBr)
+
+  const poemFinal = poemTitle + h3 + stanza
+  console.log(poemFinal)
+  return poemFinal
+}
 
 // attach a click event to #get-poem
 getPoemBtn.onclick = async function() {
